1c2335ef7d1f8ef15747c759d2e86e46254bb0bb
Manditha77 <manditha77madushanka@gmail.com>
2025-02-11 16:08:47 +0530

    Initial commit

---
+++ src/main/java/com/uok/groceryease_backend/GroceryeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uok.groceryease_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryeaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uok/groceryease_backend/GroceryeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uok.groceryease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
